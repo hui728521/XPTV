1d3c51141eb70d388f853584b96c2cde6db2e71f
hui728521 <46956166+hui728521@users.noreply.github.com>
2026-03-29 01:49:53 +0800

    Delete netflixgc.js

--- netflixgc.js
+++
@@ -1,6 +0,0 @@
-{
-  "name": "NetflixGC影视",
-  "type": 3,
-  "api": "csp_netflixgc",
-  "ext": "https://github.com/hui728521/XPTV/blob/10178af0567b182fc4df44fd37af39f9aadf7f03/js/%20netflixgc.js"
-}
